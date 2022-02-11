c261dae5527efdbaf4f8f4162010f1a3893d2612
HJ <58260711+Hannahjoosten@users.noreply.github.com>
2022-02-11 20:12:39 +0100

    Update java.js

--- scripts/java.js
+++ scripts/java.js
@@ -1,11 +1,13 @@
-<!--Menu Klik-->
+// Variables
 var button = document.getElementById("menuknop");
-<!--Artikel klik-->
 var element = document.querySelector("article");
-<!--Maakt de button zichtbaar-->
+var kruisje = document.getElementById("kruisje");
+
+// Functie om hamburger menu zichtbaar te maken of te laten verdwijnen
 button.addEventListener("click", function() {
     element.classList.toggle("zichtbaar");
 })
-<!--Maak de button zichtbaar-->
-buttton.addEventListener("click", function() {
-        element.classList.toggle("zichtbaar")}
+
+kruisje.addEventListener("click", function() {
+    element.classList.toggle("zichtbaar");
+})
